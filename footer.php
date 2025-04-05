4c8b7710177f528b78a9b7c8d2ebdcf810d20f03
serebias <serebias1169@gmail.com>
2025-04-05 09:56:00 -0300

    Add footer.php 1743857749648

---
+++ footer.php
@@ -0,0 +1,20 @@
+</main>
+    <footer class="bg-blue-600 text-white p-4 mt-8 text-center">
+        <div class="container mx-auto">
+            <p class="mb-2">© 2024 Hotel VIP - Todos os direitos reservados</p>
+            <div class="flex justify-center space-x-4">
+                <a href="#" class="hover:text-blue-300"><i class="fab fa-facebook"></i></a>
+                <a href="#" class="hover:text-blue-300"><i class="fab fa-instagram"></i></a>
+                <a href="#" class="hover:text-blue-300"><i class="fab fa-whatsapp"></i></a>
+            </div>
+            <p class="mt-2 text-sm">Imagens por <a href="https://www.pexels.com" class="text-blue-300 hover:underline">Pexels</a></p>
+        </div>
+    </footer>
+    <script>
+        // Funções JavaScript globais
+        function confirmarAcao(mensagem) {
+            return confirm(mensagem);
+        }
+    </script>
+</body>
+</html>
